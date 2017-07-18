f1e3b8694526b27d75a7aa8d7750d7f51644b840
Soupreme <lunarmoon56@gmail.com>
2017-07-18 14:12:42 -0700

    Updated the simple file sharing script to stream the image to clipboard

--- HappyLittlePythonDir/SimpleImageSharingScript.py
+++ HappyLittlePythonDir/SimpleImageSharingScript.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 import datetime
+
+#requests will be used when the screenshots are uploaded to a file server automatically
+#import requests
+
 from subprocess import call
 title = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")+".png"
 call(["import", title])
-
+call(["xclip", "-selection", "clipboard", "-t", "image/png", title])
